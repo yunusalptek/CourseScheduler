4206d5892b308b65bc5cbf5cb212ef4a0d6a81ef
Yunus Alptekinoğlu <yunus.alptekinoglu@gmail.com>
2023-12-20 14:35:32 -0500

    CourseQueries

---
+++ CourseQueries.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Yunus
+ */
+
+public class CourseQueries {
+    
+    private static Connection connection;
+    private static PreparedStatement addCourse;
+    private static PreparedStatement getAllCourseCodes;
+    private static ResultSet resultSet;
+    
+    public static void addCourse(CourseEntry course) {
+        connection = DBConnection.getConnection();
+        try
+        {
+            addCourse = connection.prepareStatement("insert into app.course (courseCode, description) values (?, ?)");
+            addCourse.setString(1, course.getCourseCode());
+            addCourse.setString(2, course.getDescription());
+            addCourse.executeUpdate();
+        }
+        catch(SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+        }
+    }
+    
+    public static ArrayList<String> getAllCourseCodes() {
+        connection = DBConnection.getConnection();
+        ArrayList<String> courseCodes = new ArrayList<String>();
+        try
+        {
+            getAllCourseCodes = connection.prepareStatement("select coursecode from app.course");
+            resultSet = getAllCourseCodes.executeQuery();
+            
+            while(resultSet.next())
+            {
+                courseCodes.add(resultSet.getString(1));
+            }
+        }
+        catch(SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+        }
+        return courseCodes;
+    }
+}
